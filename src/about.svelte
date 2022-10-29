2845cce975588df43aff9b25bd642982fca33a80
givingdonation <gitlab@iscienceworld.com>
2022-10-28 19:36:19 -0700

    made site

--- src/about.svelte
+++ src/about.svelte
@@ -5,6 +5,19 @@
 
   
   <h1>About me:</h1>
+  <h2>I have been programming since approximately 2017. Ever since I have been working <br>
+    on several projects, and this website is my place to centralize my work.</h2>
+    <br> 
+    <h2>I have made numerous projects to keep learning, and have even taught others <br>
+         how to program as well. These projects are stored under the projects section.</h2>
+<br>   
+ <h2>If you would like to contact me for any reason, here are my email, hosted under my own mail server:</h2>
+ <br>
+    <h3>
+        <li>For things related to my github/lab: <a href="mailto:gitlab@iscienceworld.com">gitlab@iscienceworld.com</a></li>
+        <li>For things related to my work: <a href="mailto:work@iscienceworld.com">work@iscienceworld.com</a></li>
+        <li><a href="https://stackoverflow.com/users/20360929/givingdonation">my stack overflow account</a></li>
+    </h3>
 
 </main>
 
