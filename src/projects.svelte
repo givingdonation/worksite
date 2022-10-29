217a0983cef752c5db02c003000a201e2ee1b946
givingdonation <gitlab@iscienceworld.com>
2022-10-28 19:44:56 -0700

    finished

--- src/projects.svelte
+++ src/projects.svelte
@@ -9,7 +9,7 @@
       <li>A website from scratch, to teach a computer science concept: Lambda Math <a href='https://gitlab.com/a8615/lambda-math'>(gitlab) </a><a href="https://lambdamath.neocities.org"> (Try it!)</a></li>
       <br><li>The contributing to a new group for tutoring people in CS at my school: Java2Life <a href='https://github.com/luhi1/Java2Life'> (github) </a></li>
       <br><li>A website and course on a mathematical topic: Googology Course <a href='https://googologycourse.neocities.org/'> (Try it!)</a></li>
-      <br><li>A website for the future non-profit: Teens Against Trafficking <a href='https://github.com/givingdonation/TATwebsite'> (github) </a> <a href='https://givingdonation.github.io/TATwebsite/dist/'> (Try it!)</a></li>
+      <br><li>A potential website for the project: Teens Against Trafficking <a href='https://github.com/givingdonation/TATwebsite'> (github) </a> <a href='https://givingdonation.github.io/TATwebsite/dist/'> (Try it!)</a></li>
       <br><li>A mobile application for school scheduling (won 1st place at FBLA) <a href='https://luhi1/ChronoBound'> (github)</a></li>
       <br><li>A projects for helping people with class work, particularly a script for automation <a href='https://gitlab.com/a8615'> (gitlab) </a></li>
       <br><li>A group of small projects for learning various programming languages <a href='https://gitlab.com/learningfromsimpleprojects'> (gitlab) </a></li>
