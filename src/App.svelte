9aed3cd50430fb49dfa130b0253057824acc1e2c
givingdonation <gitlab@iscienceworld.com>
2022-10-28 18:30:33 -0700

    made site pages outline

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,30 @@
 <script lang="ts">
 import Home from "./home.svelte"
 import About from "./about.svelte"
+import Projects from "./projects.svelte"
 
-const pages:any[] = [Home, About]
+const pages:any[] = [Home, About, Projects]
   let page_index:number = 0
   //let file_name:string = ""
 
   function change_page(name:string): void{
-    if (name == "home"){
-        //file_name = file
-        page_index = 0
-      }
-    else if (name == "about"){
-        page_index = 1
-      }
+    page_index = {"home":0,"about":1,"projects":2}[name]
   }
 
 </script>
 
 <main>
-  <ul>
-    <li>
-      <button on:click={()=>change_page("home")}>Home</button>
+  <div class="navbar">
+    <li style="display:block; margin-right: auto;">
+      <button on:click={()=>change_page("home")} >Home</button>
     </li>
     <li>
       <button on:click={()=>change_page("about")}>About</button>
     </li>
     <li>
-      
+      <button on:click={()=>change_page("projects")}>Projects</button>
     </li>
-  </ul>
+  </div>
 
   <svelte:component 
     this={pages[page_index]}
@@ -40,5 +35,15 @@ const pages:any[] = [Home, About]
 </main>
 
 <style>
-
+.navbar {
+  list-style: none;
+  background-color: #121212;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-left: 0px;
+  margin-right: 0;
+  
+}
 </style>
--- src/home.svelte
+++ src/home.svelte
@@ -2,14 +2,18 @@
     </script>
     
     <main>
-    
-      <img alt="Fractal">
-      <h1>I am givingdonation, and this is my worksite!</h1>
-      <h2>My personal site for work!</h2>
-      <p>Here I have a centralized place for people to access some of the things I have done.</p>
+        <img src="/src/assets/fractal.png" alt="Fractal" style="opacity: 0.35; width: 100%;">
+      <div class="center">
+        <h1>I am Carlo, and this is my website!</h1>
+        <h2>My personal site for work!</h2>
+        <h3>Here I have a centralized place for people to access some of the things I have done.</h3>
+      </div>
+      
     
     </main>
     
     <style>
-    
+    .center{
+        position:absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);
+    }
     </style>
